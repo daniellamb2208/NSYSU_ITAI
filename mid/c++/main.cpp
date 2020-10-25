f3c9ee898515182f41c46156d1a13afa71a7a3cf
25077667 <zxc25077667@gmail.com>
2020-10-26 03:02:07 +0800

    Add dirver of tesing ants AI

---
+++ mid/c++/main.cpp
@@ -0,0 +1,12 @@
+#include "ant.hpp"
+
+int main()
+{
+    Ant myant[5] = {
+        Ant(1), Ant(2), Ant(3), Ant(4), Ant(5),
+    };
+    for (size_t i = 0; i < 5; i++) {
+        myant[i].job();
+    }
+    return 0;
+}
