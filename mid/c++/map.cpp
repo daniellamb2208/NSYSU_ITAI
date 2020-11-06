bb640bc50f3e8bfec519c7e0dcb5fd2d3f4fde9a
25077667 <zxc25077667@gmail.com>
2020-11-07 02:14:23 +0800

    Add objPool and atomically

--- mid/c++/map.cpp
+++ mid/c++/map.cpp
@@ -1,6 +1,7 @@
 #include "map.hpp"
 #include <array>
 #include <functional>
+#include <numeric>
 #include <random>
 #include <vector>
 using namespace std;
@@ -10,6 +11,8 @@ static vector<double> separateNormally(size_t num, double value)
 {
     vector<double> result;
     // TODO: separate `value` into `num` parts by normal distribution
+    for (size_t i = 0; i < num; i++)
+        result.at(i) = value / num;  // Use uniform currently
     return result;
 }
 
@@ -25,6 +28,7 @@ inline double getRand()
 LocalMap::LocalMap()
 {
     this->t = thread(sync);
+    this->totalFoods = 0;
 }
 
 LocalMap::~LocalMap()
@@ -39,29 +43,65 @@ MapObj LocalMap::get_at(pos_t pos)
 
 void LocalMap::put_at(pos_t pos, MapObj thing)
 {
+    double preValue = get_at(pos).value;
     (this->arr.at(pos.x).at(pos.y)).store(thing);
+
+    // Add to objPool, just overwrite it
+    // It's needless to free orig obj
+    this->objPool[thing.type][pos] = thing;
+
+    // Update objPool
+    double postValue = get_at(pos).value;
+    double origTotalFoods = this->totalFoods.load();
+    this->totalFoods.store(origTotalFoods + (postValue - preValue));
 }
 
 void LocalMap::merge(pos_t pos, MapObj _source)
 {
-    // me is a lvalue of MapObj
-    auto me = this->arr.at(pos.x).at(pos.y).load();
+    // me is a rvalue of MapObj
+    auto me = this->get_at(pos);
     // Put at home or type is same
-    if (me.type == HOME || _source.type == me.type) {
+    if (me.type == HOME || _source.type == me.type)
         me.value += _source.value;
-    } else {
-        // one is FOOD, one is PHEROMONE
-        // FOOD + PHEROMONE = FOOD, all the PHEROMONE will lost
-        me.value = (_source.type == FOOD) ? _source.value : me.value;
+
+    // one is FOOD
+    // FOOD + PHEROMONE = FOOD, all the PHEROMONE will lost
+    // Bitwise operation, don't touch it if you don't know.
+    switch (me.type | _source.type) {
+    case (FOOD | PHEROMONE):
+        me.value = (me.type & FOOD) * me.value;
+        _source.value = (_source.type & FOOD) * _source.value;
+    case (FOOD):
         me.type = FOOD;
+        break;
+    case (PHEROMONE):
+        me.type = PHEROMONE;
+        break;
+    default:
+        throw "TypeError: merge";
+        break;  // Undefined behavior, impossible execute here
     }
-    this->arr.at(pos.x).at(pos.y).store(me);
+    me.value += _source.value;
+    // Give `put_at` to process the objPool problem
+    this->put_at(pos, me);
 }
 
-pos_t findClosest(pos_t currentPos, int type)
+pos_t LocalMap::findClosest(const pos_t &currentPos, char type)
 {
-    // TODO: implement this function, might create a TREE in `LocalMap` class
-    return pos_t();
+    // Designated initializers, after C++20
+    pos_t pos = {.x = HEIGHT, .y = WIDTH};
+    auto minLen = INFINITY;
+
+    // C++17 structured bindings
+    for (auto &[_pos, obj] : this->objPool[type]) {
+        double len = currentPos - _pos;
+        if (len < minLen) {
+            minLen = len;
+            pos = _pos;
+        }
+    }
+
+    return pos;
 }
 
 // Default callee
@@ -70,7 +110,7 @@ void foodGenerator(LocalMap &_map)
     auto value = getRand() * MAX_FOOD * 0.03;  // Up to 3% of MAX
 
     // Designated initializers, after C++20
-    pos_t pos = {.x = getRand() * HEIGHT, .y = getRand() * WIDTH};
+    pos_t pos = {.x = int(getRand() * HEIGHT), .y = int(getRand() * WIDTH)};
 
     size_t num = getRand() * 50;  // 50 food? I guess.
     foodGenerator(pos, num, value, _map);
@@ -79,8 +119,8 @@ void foodGenerator(LocalMap &_map)
 // Separeate foods uniformly
 void foodGenerator(pos_t _pos, size_t num, double value, LocalMap &_map)
 {
-    // TODO: calculate the total foods(might use some tree)
-    if (value + __TOTAL_FOOD__ > MAX_FOOD)
+    // FIXME: No, we cannot add them all
+    if (value + _map.totalFoods.load() > MAX_FOOD)
         return;
     auto values = separateNormally(num, value);
     for (auto i : values)
--- mid/c++/map.hpp
+++ mid/c++/map.hpp
@@ -2,8 +2,11 @@
 #define __MAP_HPP__
 #include <array>
 #include <atomic>
+#include <cmath>
 #include <iterator>
+#include <map>
 #include <thread>
+#include <vector>
 #define HEIGHT 600
 #define WIDTH 800
 #define DISAPPEAR_THRESHOLD 0.3
@@ -12,22 +15,31 @@
 const int DISCOUNT_LAMBDA = 1.1;
 using namespace std;
 
-typedef struct {
+enum { EMPTY, FOOD, PHEROMONE, HOME };
+
+typedef struct _p {
     int x;
     int y;
+    // Length of distance
+    friend double operator-(const struct _p(&a), const struct _p(&b))
+    {
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    }
+    friend bool operator<(const struct _p(&a), const struct _p(&b))
+    {
+        return (a.x * a.x) + (a.y * a.y) < (b.x * b.x) + (b.y * b.y);
+    }
 } pos_t;
-
-enum { EMPTY, FOOD, PHEROMONE, HOME };
 
 class MapObj
 {
 public:
     MapObj()
     {
-        this->food = 0;
+        this->value = 0;
         this->type = EMPTY;
     }
-    MapObj(double _value) : value(_value), type(FOOD) {}
+    MapObj(double _value, char _type = FOOD) : value(_value), type(_type) {}
     void clean()
     {
         this->type = EMPTY;
@@ -44,8 +56,14 @@ public:
 class LocalMap
 {
 private:
+    static atomic<double> totalFoods;
     // This map is a static 2D array of atomic obj
     static array<array<atomic<MapObj>, WIDTH>, HEIGHT> arr;
+    // This is a naive way to find the closest obj
+    // Store all PHEROMONE and FOOD's position to this obj pool
+    // And brute force linear searching all obj
+    // access by objPool[FOOD][pos];
+    map<char, map<pos_t, MapObj>> objPool;
     thread t;
     // Sync this map obj, create a thread to run it in background
     static void sync();
@@ -73,6 +91,11 @@ public:
                               LocalMap &_map);
     // Default call this function
     friend void foodGenerator(LocalMap &_map);
+
+    // TODO: find some object is closest in the `type`
+    // There is some algorithm called "Space Partitioning"
+    // https://stackoverflow.com/questions/28154450/find-nearest-object-in-2d-can-it-be-optimised-below-on/28155167#28155167
+    pos_t findClosest(const pos_t &currentPos, char type);
 };
 
 // Must be static member function
@@ -82,21 +105,23 @@ void LocalMap::sync()
     // iterator is the operand named [ij]ter
     for (auto iter = arr.begin(); iter != arr.end(); iter++)
         for (auto jter = iter->begin(); jter != iter->end(); jter++) {
-            if (jter->load().type != EMPTY && jter->load().value < 0.03)
-                jter->load().clean();
-            // PHEROMONE will dissipate
-            if (jter->load().type == PHEROMONE) {
-                auto me = jter->load();
-                me.value /= DISCOUNT_LAMBDA;
-                jter->store(me);
-            }
+            auto obj = jter->load();
+            if (obj.type != EMPTY) {
+                auto preValue = obj.value;
+                if (obj.value < 0.03)
+                    obj.clean();
+                // PHEROMONE will dissipate
+                if (obj.type == PHEROMONE) {
+                    auto me = obj;
+                    me.value /= DISCOUNT_LAMBDA;
+                    jter->store(me);
+                }
+                // Update totalFoods
+                auto origTotalFoods = totalFoods.load();
+                totalFoods.store(origTotalFoods + jter->load().value -
+                                 preValue);
+            }  // sparse array skip the EMPTY directly
         }
 }
-
-// TODO: find some object is closest in the `type`
-// There is some algorithm called "Space Partitioning"
-// https://stackoverflow.com/questions/28154450/find-nearest-object-in-2d-can-it-be-optimised-below-on/28155167#28155167
-pos_t findClosest(pos_t currentPos, int type);
-
 
 #endif
