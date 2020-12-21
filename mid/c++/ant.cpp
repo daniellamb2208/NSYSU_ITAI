5e561bc3f7fe0d66d777e20153fe811359eb4930
25077667 <zxc25077667@gmail.com>
2020-12-21 21:51:31 +0800

    Finish the ant Job interfaces

--- mid/c++/ant.cpp
+++ mid/c++/ant.cpp
@@ -1,8 +1,12 @@
 #include "ant.hpp"
 #include <functional>
+#include <memory>
 #include <random>
 using namespace std;
 #define QUEEN_APPETITE 100
+#define MALE_APPETITE 80
+#define WORKER_APPETITE 1
+#define SOLDIER_APPETITE 40
 
 static inline const int get_rand_uniform(int cube_sum_range = 5) noexcept
 {
@@ -14,6 +18,38 @@ static inline const int get_rand_uniform(int cube_sum_range = 5) noexcept
     mt19937_64 gen = mt19937_64(rd());
     uniform_int_distribution<int> dis(0, bound);  // default [0, 100)
     return bind(dis, gen)();                      // bind and call
+}
+
+// Go to destination for single step
+static inline void go(pos_t &curr, const pos_t &dest)
+{
+    long int x_diff = dest.x - curr.x;
+    long int y_diff = dest.y - curr.y;
+    // +1 prevent 0/0
+    curr.x +=
+        (abs(x_diff) > abs(y_diff)) ? ((x_diff | 1) / abs(x_diff | 1)) : 0;
+    curr.y +=
+        (abs(x_diff) <= abs(y_diff)) ? ((y_diff | 1) / abs(y_diff | 1)) : 0;
+
+    // Overflow condition, move one step, so if overflow is occurred
+    // previous position most be 0
+    if (curr.x & (1 << 31))
+        curr.x = 0;
+    if (curr.y & (1 << 31))
+        curr.y = 0;
+    if (curr.x >= HEIGHT)
+        curr.x = HEIGHT - 1;
+    if (curr.y >= WIDTH)
+        curr.y = WIDTH - 1;
+}
+
+static inline bool __alive_handler(Ant *me)
+{
+    // Check and set
+    if (me->get_step() == 0 || me->get_energy() == 0)
+        me->set_live_status(false);
+
+    return me->get_live_status();
 }
 
 Ant::Ant(int job_code)
@@ -38,10 +74,9 @@ Ant::Ant(int job_code)
     default:
         throw runtime_error("Wrong opcode");
     }
-    this->job = my_job.get();
+    this->job = move(my_job);
     this->my_job_code = static_cast<Job_Code>(job_code);
 }
-
 
 // Any Organism has a vocation by the God
 // The ant would have the selected vocation.
@@ -52,6 +87,8 @@ void Queen::pregnant(Job_Code _gift)
 
 void Queen::work()
 {
+    // Each work comsume a step.
+    me->set_step(me->get_step() - 1);
     // Get the closest of floor(i^3) form the opcode
     auto cube_convertion = [](int opcode) {
         // Loop over enum class from the end
@@ -62,6 +99,7 @@ void Queen::work()
         }
     };
 
+    // Pregnant process
     if (bleed()) {
         // The probability of Job_code is:
         // queen : 0%
@@ -74,6 +112,15 @@ void Queen::work()
         pregnant(static_cast<Job_Code>(opcode));
     } else {
         pregnant(Job_Code::male);
+    }
+
+    remove_dead_ant();
+}
+
+void Queen::alive_handler()
+{
+    if (!__alive_handler(me)) {
+        // TODO: move my_slave to virgin
     }
 }
 
@@ -98,13 +145,145 @@ int Queen::get_food()
     // TODO: Is it enough food to take from home?  Check it!
     MapObj consumption = MapObj(-QUEEN_APPETITE);
     my_map->merge(me->at(), consumption);
-    return 100;
+    // Add to my energy
+    me->set_energy(me->get_energy() + QUEEN_APPETITE);
+    return QUEEN_APPETITE;
+}
+
+void Queen::remove_dead_ant()
+{
+    for (auto iter = my_slave.begin(); iter != my_slave.end();) {
+        if (!iter->get_live_status())
+            iter = my_slave.erase(iter);
+        else
+            iter++;
+    }
 }
 
 void Male::work()
 {
-    // TODO: Walk around my_queen
+    me->set_step(me->get_step() - 1);
     pos_t my_queen_pos = this->me->get_queen_pos();
     pos_t my_pos = this->me->at();
+
+    auto entropy = get_rand_uniform() * 3 / 100.0;
+    if (distance(my_pos, my_queen_pos) < entropy) {
+        // Too close, leave more
+        pos_t target(getRand() * HEIGHT, getRand() * WIDTH);
+        go(this->me->at(), target);
+    } else {
+        go(this->me->at(), my_queen_pos);
+    }
 }
 
+int Male::get_food()
+{
+    // TODO: Check have food at home
+    // TODO: Eat the food, and Add it to my energy
+    return MALE_APPETITE;
+}
+
+void Male::alive_handler()
+{
+    __alive_handler(me);
+}
+
+void Virgin::work()
+{
+    // Queen move her slave to me
+    if (this->tmp_slave.size() && me->at() != new_home) {
+        // TODO: Move to new house
+    } else if (this->tmp_slave.size() && me->at() == new_home) {
+        // TODO: Create a queen job, move slave to it, and delete this
+        auto new_job = make_unique<Queen>(me);
+        new_job->get_slave().swap(this->tmp_slave);  // Move ir
+        me->set_job(move(new_job));                  // FIXME: might bug here
+    } else {
+        // Sleep, do nothing, not reduce my step
+    }
+}
+
+void Virgin::alive_handler()
+{
+    __alive_handler(me);
+}
+
+void Worker::work()
+{
+    if (is_go_to_find_food && my_food.type == EMPTY)
+        find_food();
+    else if (my_food.type == EMPTY)
+        my_food = pick_food();
+    else
+        return_home();
+}
+
+void Worker::put_pheromone(pos_t pos)
+{
+    me->get_map()->merge(pos, MapObj(100, PHEROMONE));
+}
+
+// Be careful! Here is eat the food from my hand
+// Not pick up the food from env.
+int Worker::get_food()
+{
+    if (my_food.type == FOOD) {
+        my_food.value--;
+        me->set_energy(me->get_energy() + WORKER_APPETITE);
+    }
+    return WORKER_APPETITE;
+}
+
+void Worker::alive_handler()
+{
+    __alive_handler(me);
+}
+
+void Worker::find_food()
+{
+    me->set_step(me->get_step() - 1);
+    // TODO: go to find food, Use the dot product algorithm
+    // TODO: If found food, set is_go_to_find_food to false
+}
+
+MapObj Worker::pick_food()
+{
+    auto my_map = me->get_map();
+    // FIXME: have race condition here
+    MapObj picked_up = my_map->get_at(me->at());
+    my_map->merge(me->at(), MapObj(-picked_up.value, FOOD));
+    return picked_up;
+}
+
+void Worker::return_home()
+{
+    me->set_step(me->get_step() - 1);
+    if (my_food.value == 0) {
+        // I had eaten the food when I take it back.
+        my_food.type = EMPTY;
+        is_go_to_find_food = true;
+    }
+    // TODO: Go home
+}
+
+void Soldier::work()
+{
+    me->set_step(me->get_step() - 1);
+    pos_t q_pos = me->get_queen_pos();
+    pos_t entropy(get_rand_uniform() / 49, get_rand_uniform() / 49);
+    pos_t target = q_pos + entropy;
+    go(me->at(), target);
+}
+
+int Soldier::get_food()
+{
+    // TODO: Check have food at home
+    // TODO: Eat the food, and Add it to my energy
+    // TODO: same as Male
+    return SOLDIER_APPETITE;
+}
+
+void Soldier::alive_handler()
+{
+    __alive_handler(me);
+}
--- mid/c++/ant.hpp
+++ mid/c++/ant.hpp
@@ -49,24 +49,21 @@ protected:
     // Descenting energy, can be increased by eat food, but will die if count to
     // zero.
     int energy = MAXENERGY;
+    bool is_alive = true;
 
 
     friend class Job;  // Make Job could access *this directly
-    Job *job = nullptr;
+    unique_ptr<Job> job = nullptr;
 
 public:
     Ant() = delete;
     Ant(int job_code);  // set job by number,
     Ant(Job *_job = nullptr) : job(_job) {}
-    ~Ant() { delete this->job; }
+    ~Ant() = default;
     pos_t &at() { return this->pos; }
     void set_step(int _step) { this->step = _step; }
     const int get_step() const { return this->step; }
-    void set_job(Job *_job)
-    {
-        delete this->job;
-        this->job = _job;
-    }
+    void set_job(unique_ptr<Job> &&_job) { this->job = move(_job); }
     void set_map(LocalMap *_myMap) { this->myMap = _myMap; }
     LocalMap *get_map() { return this->myMap; }
     void set_queen_pos(Ant *queen) { this->my_queen_pos = queen->at(); }
@@ -74,6 +71,8 @@ public:
     int get_energy() const { return this->energy; }
     void set_energy(int _value) { this->energy = _value; }
     Job_Code get_job_code() { return this->my_job_code; }
+    bool get_live_status() { return this->is_alive; }
+    void set_live_status(bool status) { this->is_alive = status; }
 };
 
 // -----------------
@@ -81,6 +80,7 @@ public:
 class Job
 {
 protected:
+    // Including walk, job, management
     virtual void work() = 0;
     // Eat the food which it get
     virtual void eat() = 0;
@@ -91,6 +91,8 @@ protected:
     // This function will consume food from env. to itself.
     // If not found, return 0.
     virtual int get_food() = 0;
+    // Set live status in each work being done
+    virtual void alive_handler() = 0;
 
 public:
     virtual ~Job() {}
@@ -99,6 +101,7 @@ public:
     {
         work();
         eat();
+        alive_handler();
     }
 };
 
@@ -111,65 +114,85 @@ class Queen : public Job
     bool bleed() { return find_male(); }
     void pregnant(Job_Code _gift);  // job code with gift
     void work() final;
-    void eat() final { me->set_energy(me->get_energy() - 100); }
+    void eat() final { me->set_energy(me->get_energy() - get_food()); }
     int get_food() final;
+    void alive_handler() final;
+    void remove_dead_ant();
 
     // Warning: Queen's my_queen_pos is undefined!
 
 public:
     Queen(Ant *_me = nullptr) : me(_me) {}
-    ~Queen();
+    ~Queen() = default;
+    vector<Ant> &get_slave() { return this->my_slave; };
 };
 
 class Male : public Job
 {
     Ant *me;
     void work() final;
-    void eat() final { me->set_energy(me->get_energy() - 80); }
+    void eat() final { me->set_energy(me->get_energy() - get_food()); }
     int get_food() final;
+    void alive_handler() final;
 
 public:
     Male(Ant *_me = nullptr) : me(_me) {}
-    ~Male();
+    ~Male() = default;
 };
 
 class Virgin : public Job
 {
     Ant *me;
+    vector<Ant> tmp_slave;
+    pos_t new_home;
     void work() final;
     // Eat nothing
     void eat() final {}
     int get_food() final {}
+    void alive_handler() final;
 
 public:
     Virgin(Ant *_me = nullptr) : me(_me) {}
-    ~Virgin();
+    ~Virgin() = default;
+    // FIXME: there is another issue about moving the home
+    // Where is the slave of this Virgin or new queen's home?
+    // The home is not the Virgin or queen's place, and need to
+    // remove the label of home on the map
+    void set_new_home(pos_t pos) { this->new_home = pos; }
 };
 
 
 class Worker : public Job
 {
+    bool is_go_to_find_food = true;
+    MapObj my_food = MapObj();
     Ant *me;
+
     void work() final;
-    void eat() final { me->set_energy(me->get_energy() - 1); }
+    void eat() final { me->set_energy(me->get_energy() - get_food()); }
     void put_pheromone(pos_t pos);
     int get_food() final;
+    void alive_handler() final;
+    void find_food();
+    MapObj pick_food();
+    void return_home();
 
 public:
     Worker(Ant *_me = nullptr) : me(_me) {}
-    ~Worker();
+    ~Worker() = default;
 };
 
 class Soldier : public Job
 {
     Ant *me;
     void work() final;
-    void eat() final;
+    void eat() final { me->set_energy(me->get_energy() - get_food()); }
     int get_food() final;
+    void alive_handler() final;
 
 public:
     Soldier(Ant *_me = nullptr) : me(_me) {}
-    ~Soldier();
+    ~Soldier() = default;
 };
 
 
